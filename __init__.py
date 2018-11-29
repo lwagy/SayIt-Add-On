55d8dcc9f33457e1c63ff6e73c96e547c17b0c31
Lawrence Wagy <lnwagy@gmail.com>
2018-11-29 13:49:09 -0600

    Plugin with Google API and gTTS.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,21 @@
-from thread import start_new_thread
-from modules import app, cbpi
-import requests
-import logging
-import pyttsx
+import re
+import os
 import time
+import logging
+import requests
+import unicodedata
+from os import path
+from gtts import gTTS
+from pygame import mixer
+from modules import app, cbpi, DBModel
+from thread import start_new_thread
 
-SayIt_Volume = None
-SayIt_Voice = None
+mixer.init()
 SayIt = None
-TTS = pyttsx.init()
-
+SayIt_Voice = None
+SayIt_Volume = None
+RootDir = os.path.dirname(os.path.abspath(__file__))
+SayItLang = [["el", "Greek"], ["zh-cn", "Chinese (Mandarin/China)"], ["my", "Myanmar (Burmese)"], ["mr", "Marathi"], ["pt-br", "Portuguese (Brazil)"], ["ml", "Malayalam"], ["uk", "Ukrainian"], ["en-in", "English (India)"], ["en-nz", "English (New Zealand)"], ["de", "German"], ["sk", "Slovak"], ["mk", "Macedonian"], ["sv", "Swedish"], ["sw", "Swahili"], ["no", "Norwegian"], ["sq", "Albanian"], ["sr", "Serbian"], ["pt-pt", "Portuguese (Portugal)"], ["te", "Telugu"], ["km", "Khmer"], ["en-ca", "English (Canada)"], ["ja", "Japanese"], ["hu", "Hungarian"], ["fi", "Finnish"], ["bs", "Bosnian"], ["hi", "Hindi"], ["es-us", "Spanish (United States)"], ["da", "Danish"], ["ko", "Korean"], ["jw", "Javanese"], ["bn", "Bengali"], ["en-ie", "English (Ireland)"], ["hr", "Croatian"], ["en-tz", "English (Tanzania)"], ["hy", "Armenian"], ["fr", "French"], ["ta", "Tamil"], ["pl", "Polish"], ["is", "Icelandic"], ["ro", "Romanian"], ["en-za", "English (South Africa)"], ["th", "Thai"], ["tl", "Filipino"], ["si", "Sinhala"], ["lv", "Latvian"], ["ne", "Nepali"], ["tr", "Turkish"], ["en-gh", "English (Ghana)"], ["zh-tw", "Chinese (Mandarin/Taiwan)"], ["en-uk", "English (UK)"], ["la", "Latin"], ["pt", "Portuguese"], ["en-ph", "English (Philippines)"], ["nl", "Dutch"], ["ru", "Russian"], ["en-gb", "English (UK)"], ["es", "Spanish"], ["id", "Indonesian"], ["es-es", "Spanish (Spain)"], ["et", "Estonian"], ["cs", "Czech"], ["su", "Sundanese"], ["fr-fr", "French (France)"], ["ar", "Arabic"], ["cy", "Welsh"], ["en-ng", "English (Nigeria)"], ["it", "Italian"], ["fr-ca", "French (Canada)"], ["en-us", "English (US)"], ["ca", "Catalan"], ["vi", "Vietnamese"], ["af", "Afrikaans"], ["en-au", "English (Australia)"], ["en", "English"], ["eo", "Esperanto"]]
 
 
 @cbpi.initalizer(order=9000)
@@ -17,44 +23,32 @@ def init(cbpi):
     print("Initializing the SayIt plugin by Lawrence Wagy")
     cbpi.app.logger.info("Initializing the SayIt plugin by Lawrence Wagy")
     global SayIt
-    cbpi.app.logger.info("Initialize SayIt Plugin")
+
     SayItVoice()
     SayItVolume()
     if SayIt_Voice is None or not SayIt_Voice:
         cbpi.notify("SayIt Plugin Error", "Check the SayIt voice parameter", type="Warning", timeout=None)
     else:
-        voices = TTS.getProperty('voices')
-        for voice in voices:
-            if SayIt_Voice == voice.name:
-                TTS.setProperty('voice', voice.id)
-                try:
-                    TTS.setProperty('volume', float(SayIt_Volume)/9)
-                except:
-                    cbpi.notify("SayIt Error", "Unable to set the volume.", type="Warning", timeout=None)
-                    cbpi.app.logger.error("SayIt Error - Unable to set the volume.")
-        TTS.setProperty('rate', 150)
+        try:
+           mixer.music.set_volume(float("{0:.1f}".format(float(SayIt_Volume)/9)))
+        except:
+            cbpi.notify("SayIt Error", "Unable to set the volume.", type="Warning", timeout=None)
+            return
         SayIt = "SayIt Initialized"
-        TTS.say(SayIt)
-        
-        
+        messageEvent({'headline': '', 'id': '', 'message': 'SayIt Plugin has been Initialized. Volume level ' + SayIt_Volume, 'timeout': None, 'type': ''})
 
-
+            
 def SayItVoice():
     global SayIt_Voice
     SayIt_Voice = cbpi.get_config_parameter("SayIt_Voice", None)
     if SayIt_Voice is None:
         try:
             VoiceOptions = []
-            voices = TTS.getProperty('voices')
-            
-            for voice in voices:
-                VoiceOptions.insert(1,voice.name)
-            
+            for key, value in SayItLang:
+                VoiceOptions.insert(1, value)
             cbpi.add_config_parameter("SayIt_Voice", "", "select", "SayIt Voice Selection", VoiceOptions)
         except:
             cbpi.notify("SayIt Error", "Unable to update the database. Update the CraftBeerPi and try rebooting.", type="Warning", timeout=None)
-            cbpi.app.logger.error("SayIt Error -Unable to update the database. Update the CraftBeerPi and try rebooting.")
-            
 
 
 def SayItVolume():
@@ -68,14 +62,33 @@ def SayItVolume():
         except:
             cbpi.notify("SayIt Error", "Unable to update the database. Update the CraftBeerPi and try rebooting.", type="Warning", timeout=None)
             cbpi.app.logger.error("SayIt Error - Unable to update the database. Update the CraftBeerPi and try rebooting.")
-            
+
+def CleanFileName(value):
+    value = str(value).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w]', '', value)
 
 
 @cbpi.event("MESSAGE", async=True)
 def messageEvent(message):
-    if SayIt == None: return
-    SayIt_Message ='{0},{1},{2}'.format(message["type"], message["headline"], message["message"])
-    TTS.say(SayIt_Message)
-    TTS.runAndWait()
-    #TTS.startLoop()
-    #TTS.endLoop()
+    if SayIt is not None:
+        SayIt_Message ='{0}, {1}, {2}'.format(message["type"], message["headline"], message["message"])
+    
+        for key, value in SayItLang:
+            if value == SayIt_Voice:
+                tts = gTTS(SayIt_Message, lang=key)
+
+        TypeDir = os.path.join(RootDir, 'Notifications' , SayIt_Voice, CleanFileName(message["type"]))
+        AudioFile = CleanFileName(message["message"])
+        AudioFilePath = os.path.join(TypeDir, AudioFile)
+        if os.path.exists(TypeDir) == False:
+            os.makedirs(TypeDir)
+        try:
+            if os.path.exists(AudioFilePath) == False:
+                tts.save(AudioFilePath)
+        except Exception as ex:
+            os.remove(AudioFilePath)
+            print('SayIt - Unable to save audio file: ' + ex.message)
+            return
+
+        mixer.music.load(AudioFilePath)
+        mixer.music.play()
